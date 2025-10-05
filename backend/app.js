76b10585f912ce4d5d9a9f8f8387f80689274e84
Stephenasembo <165886904+Stephenasembo@users.noreply.github.com>
2025-10-05 15:02:19 +0300

    Feat: Integrate Telegram Bot
    The bot can now receive commands and send back messages

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
 require("dotenv").config()
 const express = require("express")
-
 const PORT = process.env.PORT || 3000;
-
-const app = express()
+const app = express();
+const TelegramBot = require("node-telegram-bot-api");
+const telegramToken = process.env.TG_BOT;
+const bot = new TelegramBot(telegramToken, {polling: true});
 
 app.get("/", (req, res) => {
   res.send("Hello welcome to joke404.")
 })
 
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const resp = "Hello welcome to joke404. The hub of jokes."
+
+  bot.sendMessage(chatId, resp);
+});
+
+
 app.listen(PORT, () => {
-  console.log(`App is live on port ${PORT}`)
+  console.log(`App is live on port ${PORT}\nVisit http://localhost:${PORT}`)
 })
